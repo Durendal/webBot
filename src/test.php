ac94059bf8f8efb01b0670974929db6cec2b002a
Durendal <durendal@durendals-domain.com>
2015-05-03 16:08:28 -0600

    Updates

--- src/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "FTPBot.php";
-
-$bot = new FTPBot("durendal", "*assfuck420", "192.168.1.42", 21);
-$bot->download("/home/durendal/cannmine.sh", "/home/durendal/cannmine.sh");
-var_dump($bot->ls("home/durendal"));
-
-
-?>
