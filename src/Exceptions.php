3a412495fae9f7516e5c41972322966a7bd32df5
Durendal <sir.durendal@gmail.com>
2019-08-06 00:24:46 -0600

    Reorganized Exceptions

--- src/Exceptions.php
+++ src/Exceptions.php
@@ -3,31 +3,31 @@
 class webBotException extends Exception {}
 
 class ProxyException extends webBotException {}
+class SessionException extends webBotException {}
+class ParserException extends webBotException {}
+class RequestException extends webBotException {}
+class ResponseException extends  webBotException {}
+class cURLHandleException extends webBotException {}
+class CookiesException extends webBotException {}
+class HeadersException extends webBotException {}
+
+// Proxy Exceptions
 class InvalidProxyHostException extends ProxyException {}
 class InvalidProxyPortException extends ProxyException {}
 class InvalidProxyTypeException extends ProxyException {}
 class UninitializedProxyException extends ProxyException {}
 
-class CookiesException extends webBotException {}
+// Cookie Exceptions
 class InvalidCookieKeyException extends CookiesException {}
 class DuplicateCookieException extends CookiesException {}
 class UninitializedCookieException extends CookiesException {}
 
-class HeadersException extends webBotException {}
+// Header Exceptions
 class InvalidHeaderException extends HeadersException {}
 class DuplicateHeaderException extends HeadersException {}
 class UninitializedHeadersException extends HeadersException {}
 
-class SessionException extends webBotException {}
-
-class ParserException extends webBotException {}
-
-class RequestException extends webBotException {}
-
-class ResponseException extends  webBotException {}
-
-class cURLHandleException extends webBotException {}
-
+// cURL Handle Exceptions
 class URLException extends cURLHandleException {}
 class InvalidURLException extends URLException {}
 class MissingURLException extends URLException {}
